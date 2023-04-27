15114b03f845e1d4a3f3c25eda385247cb44a0a0
henriqueleandrosilva <UG_TALENTOS_C08100O@asav.brm>
2023-04-27 08:59:40 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,46 @@
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+//O código irá mostrar a variável b = 10 e a variável a = 10  e b = 5 juntas.
+
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+// O código mostra que todas as variáveis irão resultar em 10.
+
+// let HorasTrabalhadas= prompt("Quantas horas você trabalha por dia?")
+// let SalarioDia = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${SalarioDia/HorasTrabalhadas} por hora`)
+
+// let Nome 
+// let Idade
+
+// console.log(typeof Nome)
+//  console.log(typeof Idade)
+
+// Nome= String (prompt("Digite seu nome"))
+// Idade=Number (prompt("Digite sua idade"))
+
+// console.log(typeof Nome)
+//  console.log(typeof Idade)
+//  console.log("Olá", "Meu nome é", Nome, "e eu tenho", Idade, "anos" )
+
+ //O código imprime o nome e  a idade sem atribuir valor, logo em seguida ele imprime os mesmos atribuindo seus respecrivos tipos numer e string.
+
+
+// let Trabalho = prompt("Você trabalha? sim/não")
+// let idade = prompt("Você tem mais de 18 anos? sim/não")
+// let Pais= prompt("você é Brasileiro(a) sim/não")
+// console.log("Você trabalha? ",Trabalho,"Você tem mais de 18 anos? ",idade, "você é Brasileiro(a)", idade)
+
